5d350db89416b131cda19a9bc1ecbcd574bd5d01
faamaral <fabianoamaral445@gmail.com>
2021-04-28 16:05:52 -0300

    first commit

---
+++ controllers/MyiidiumController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\Controllers;
+
+use yii\web\Controller;
+
+class MyiidiumController extends Controller
+{
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
